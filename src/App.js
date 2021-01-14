7288d05dd87336069292e00659ca369cadef0e99
Michal Czyzowicz <czyzowicz.m@gmail.com>
2021-01-14 02:24:59 +0100

    add axios lib and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
-  const [pokemon, setPokemon] = useState (['bulbasaur', 'charmander']);
+  const [pokemon, setPokemon] = useState ([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
+      setPokemon(res.data.results.map(poke => poke.name))
+    })
+    return () => cancel()
+  }, [currentPageUrl]);
+
+  if(loading) return "Loading...";
   
   return (
     <PokemonList pokemon= {pokemon} />
